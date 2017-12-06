f897d22662b842cac6f601202e79621e406bf32e
LIYANGSHUAI <liyangshuai163@163.com>
2017-12-06 16:46:52 +0800

    1.0.0

--- LYSKit/Bases/LYSKVOManager.m
+++ LYSKit/Bases/LYSKVOManager.m
@@ -9,13 +9,12 @@
 #import "LYSKVOManager.h"
 
 #define LYActionDict @"LYActionDict"
-
-#import "LYSRuntimeManager.h"
+#import <objc/runtime.h>
 @implementation LYSKVOManager
 
 + (void)addObserverToObject:(id)object forKeyPath:(NSString *)keyPath action:(void(^)(id oldValue,id newValue))action identifier:(NSString *)identifier
 {
-    NSDictionary *dict = [LYSRuntimeManager associationPropertyName:LYActionDict toObject:object];
+    NSDictionary *dict = [self associationPropertyName:LYActionDict toObject:object];
     dict = dict ? [NSMutableDictionary dictionaryWithDictionary:dict] : [NSMutableDictionary dictionary];
     if ([[dict allKeys] containsObject:keyPath])
     {
@@ -25,18 +24,18 @@
     }else
     {
         [(NSMutableDictionary *)dict setObject:@{identifier:action} forKey:keyPath];
-        [LYSRuntimeManager addMethodForClass:[object class] fromClass:self instanceSel:@selector(observeValueForKeyPath:ofObject:change:context:)];
-        [LYSRuntimeManager addMethodForClass:[object class] fromClass:self instanceSel:NSSelectorFromString(@"dealloc")];
+        [self addMethodForClass:[object class] fromClass:self instanceSel:@selector(observeValueForKeyPath:ofObject:change:context:)];
+        [self addMethodForClass:[object class] fromClass:self instanceSel:NSSelectorFromString(@"dealloc")];
         [object addObserver:object forKeyPath:keyPath options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:nil];
     }
-    [LYSRuntimeManager associationPropertyName:LYActionDict value:dict toObject:object];
+    [self associationPropertyName:LYActionDict value:dict toObject:object];
 }
 
 + (void)removeObserverToObject:(id)object
                        forKeyPath:(NSString *)keyPath
                        identifier:(NSString *)identifier
 {
-    NSDictionary *dict = [LYSRuntimeManager associationPropertyName:LYActionDict toObject:object];
+    NSDictionary *dict = [self associationPropertyName:LYActionDict toObject:object];
     dict = dict ? [NSMutableDictionary dictionaryWithDictionary:dict] : [NSMutableDictionary dictionary];
     if ([[dict allKeys] containsObject:keyPath])
     {
@@ -48,11 +47,11 @@
             {
                 [object removeObserver:object forKeyPath:keyPath];
                 [(NSMutableDictionary *)dict removeObjectForKey:keyPath];
-                [LYSRuntimeManager associationPropertyName:LYActionDict value:dict toObject:object];
+                [self associationPropertyName:LYActionDict value:dict toObject:object];
             }else
             {
                 [(NSMutableDictionary *)dict setObject:identifierDict forKey:keyPath];
-                [LYSRuntimeManager associationPropertyName:LYActionDict value:dict toObject:object];
+                [self associationPropertyName:LYActionDict value:dict toObject:object];
             }
         }
     }
@@ -63,7 +62,7 @@
                         change:(NSDictionary<NSString *,id> *)change
                        context:(void *)context
 {
-    NSDictionary *dict = [LYSRuntimeManager associationPropertyName:LYActionDict toObject:object];
+    NSDictionary *dict = [self associationPropertyName:LYActionDict toObject:object];
     if (dict && [[dict allKeys] containsObject:keyPath])
     {
         for (NSString *identifier in [dict objectForKey:keyPath])
@@ -76,10 +75,30 @@
 
 - (void)dealloc
 {
-    NSDictionary *dict = [LYSRuntimeManager associationPropertyName:LYActionDict toObject:self];
+    NSDictionary *dict = [self associationPropertyName:LYActionDict toObject:self];
     if (dict) {for (NSString *keyPath in dict) {[self removeObserver:self forKeyPath:keyPath];}}
-    [LYSRuntimeManager associationPropertyName:LYActionDict value:@{} toObject:self];
+    [self associationPropertyName:LYActionDict value:@{} toObject:self];
 }
-
++ (id)associationPropertyName:(NSString *)name
+                     toObject:(id)object
+{
+    return objc_getAssociatedObject(object, (__bridge const void *)(name));
+}
++ (BOOL)addMethodForClass:(Class)forClass
+                fromClass:(Class)fromClass
+              instanceSel:(SEL)instanceSel
+{
+    IMP sel_IMP = class_getMethodImplementation(fromClass, instanceSel);
+    Method sel_Method = class_getInstanceMethod(fromClass, instanceSel);
+    const char *sel_Type = method_getTypeEncoding(sel_Method);
+    BOOL result = class_addMethod(forClass, instanceSel, sel_IMP, sel_Type);
+    return result;
+}
++ (void)associationPropertyName:(NSString *)name
+                          value:(id)value
+                       toObject:(id)object
+{
+    objc_setAssociatedObject(object, (__bridge const void *)(name), value, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
 @end
 
