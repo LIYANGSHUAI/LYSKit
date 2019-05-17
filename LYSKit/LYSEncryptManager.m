54aaf07167eae458ef450a617a1c1ae5d1174211
LIYANGSHUAI <liyangshuai163@163.com>
2019-05-17 10:47:52 +0800

    no message

--- LYSKit/LYSEncryptManager.m
+++ LYSKit/LYSEncryptManager.m
@@ -250,7 +250,7 @@
     //使用对应的CC_SHA1,CC_SHA256,CC_SHA384,CC_SHA512的长度分别是20,32,48,64
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
     //使用对应的CC_SHA256,CC_SHA384,CC_SHA512
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (int)data.length, digest);
     NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
         [output appendFormat:@"%02x", digest[i]];
